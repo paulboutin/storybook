3ff6a458e0fd3683de185ddd6dce1f7f6565fe60
Sergio del Puerto <sergio.delpuerto@intelygenz.com>
2020-06-22 01:06:00 -0700

    Remove the storybook code addon

--- .storybook/addons.js
+++ .storybook/addons.js
@@ -1,9 +1,1 @@
 import '@whitespace/storybook-addon-html/register'
-import registerAddonCode from '@whitespace/storybook-addon-code/register'
-
-registerAddonCode({
-  tabs: [
-    { label: 'SCSS', lang: 'scss' },
-    { label: 'JavaScript', lang: 'javascript', matchFiles: 'js' }
-  ]
-})
