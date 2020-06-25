703c2dafa66c11265558a3c66d2d3605db473fbd
Sergio del Puerto <sergio.delpuerto@intelygenz.com>
2020-06-25 13:05:22 -0700

    Remove code documentation from the design system stories

--- src/stories/design/Typography.js
+++ src/stories/design/Typography.js
@@ -1,15 +1,5 @@
 export const Typography = () => `
   <div class='container'>
-    <h2 class="mb-sm">Headings</h2>
-    <div class="rounded bg-grey-15 mb-base p-sm">
-      <p>
-          Headings are configured by default and require no further styling.
-      </p>
-      <p class="mt-base">Usage example</p>
-      <code>
-          <span class="pl-base">&lt;<span class="text-error">h1</span>&gt;...&lt;/<span class="text-error">h1</span>&gt;</span><br>
-      </code>
-    </div>
     <p class='text-grey-55 text-xs'>Heading 1</p>
     <h1 class='mb-base'>Where you put your money matters.<br>A bank that finances a sustainable tomorrow.</h1>
     
@@ -28,18 +18,6 @@ export const Typography = () => `
     <p class='text-grey-55 text-xs'>Heading 6</p>
     <h6 class='mb-base'>Where you put your money matters.<br>A bank that finances a sustainable tomorrow.</h6>
     
-    <h2 class="mb-sm">Base</h2>
-    <div class="rounded bg-grey-15 mb-base p-sm">
-      <p>
-          Text sizes can be modified with the class <code>.text-{size}</code>, 
-          where size is any of the following: xs, sm, md, lg, h1, h2, h3, h4, h5 or h6.
-          
-      </p>
-      <p class="mt-base">Usage example</p>
-      <code>
-          <span class="pl-base">&lt;<span class="text-error">div</span> class="<span class="text-yellow-darker">text-lg</span>"&gt;...&lt;/<span class="text-error">div</span>&gt;</span>
-      </code>
-    </div>
     <p class='text-grey-55 text-xs'>Body large</p>
     <div class='text-lg mb-base'>Where you put your money matters.<br>A bank that finances a sustainable tomorrow.</div>
     
