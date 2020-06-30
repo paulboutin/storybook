b028b27fcd09a524493ef3aa72ba75ddd6fb1d5e
Daniel Clowes <daniel.clowes@intelygenz.com>
2020-06-30 17:16:24 +0200

    implement product detail list element component

---
+++ src/stories/components/product-detail/ListElement.js
@@ -0,0 +1,55 @@
+export const ListElementWithHeader = () => `
+<section class="list-element-container">
+    <div class="list-element-header padded">
+        <p class="list-element-header-lead">Features</p>
+        <span class="list-element-header-spacer"></span>
+        <div class="list-element-header-text-block">
+          <h2 class="list-element-heading">Here are the basics</h2>
+          <p class="list-element-header-text">Manage your money while contributing with the planet</p>
+        </div>
+    </div>
+    <ul class="list-element-items">
+      <li class="list-element-item check">
+        $0 monthly service charge with any deposit per statement
+      </li>
+      <li class="list-element-item check">
+        Free access to Online Banking in our mobile app
+      </li>
+      <li class="list-element-item check">
+        No minimum balance
+      </li>
+      <li class="list-element-item check">
+        No direct deposit required
+      </li>
+    </ul>
+</section>
+`
+
+export const ListElementWithImage = () => `
+<section class="list-element-container">
+    <img class="list-element-image" src="/images/cards.png" alt="cards">
+    <div class="list-element-alt-content">
+      <div class="list-element-header">
+        <p class="list-element-header-lead">Features</p>
+        <div class="list-element-header-text-block">
+          <h2 class="list-element-heading">Here are the basics</h2>
+          <p class="list-element-header-text">Manage your money while contributing with the planet</p>
+        </div>
+      </div>
+      <ul class="list-element-items">
+        <li class="list-element-item check">
+          $0 monthly service charge with any deposit per statement
+        </li>
+        <li class="list-element-item check">
+          Free access to Online Banking in our mobile app
+        </li>
+        <li class="list-element-item check">
+          No minimum balance
+        </li>
+        <li class="list-element-item check">
+          No direct deposit required
+        </li>
+      </ul>
+    </div>
+</section>
+`
