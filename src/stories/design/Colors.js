41aa0d7e9fed4e7369a05e26ad5a64346286244b
Sergio del Puerto <sergio.delpuerto@intelygenz.com>
2020-06-30 17:50:53 -0700

    Split the colors story into smaller components to prevent code duplication

--- src/stories/design/Colors.js
+++ src/stories/design/Colors.js
@@ -1,143 +1,156 @@
 import React from 'react'
+import classNames from 'classnames'
+
+const ColorGroup = ({ children }) => (
+  <div className='col-12 md:col-3 lg:w-full p-8'>
+    <div className='m-auto max-w-xs'>{children}</div>
+  </div>
+)
+
+const ColorName = ({ children }) => (
+  <h4 className='text-grey text-sm font-semibold mb-4'>{children}</h4>
+)
+
+const ColorBlock = ({ children, backgroundColor, textColor, className }) => (
+  <div
+    className={classNames(
+      `bg-${backgroundColor} text-${textColor}`,
+      'rounded text-center text-sm py-4 mb-4',
+      className
+    )}
+  >
+    {children}
+  </div>
+)
 
 export const Colors = () => (
-  <div className='flex flex-wrap m-auto text-sm'>
-    <div className='md:flex-1 w-full p-12'>
-      <div className='m-auto max-w-xs'>
-        <h4 className='text-grey text-sm font-semibold mb-4'>Brand Green</h4>
-        <div className='bg-green text-white text-center rounded py-4 mb-4'>
-          Base
-        </div>
-        <div className='bg-green-light text-white text-center rounded py-4 mb-4'>
-          Light
-        </div>
-        <div className='bg-green-dark text-white text-center rounded py-4 mb-4'>
-          Dark
-        </div>
-        <div className='bg-green-darker text-white text-center rounded py-4'>
-          Darker
-        </div>
-      </div>
-    </div>
+  <div className='flex flex-wrap lg:flex-no-wrap'>
+    <ColorGroup>
+      <ColorName>Brand Green</ColorName>
+      <ColorBlock backgroundColor='green' textColor='white'>
+        Base
+      </ColorBlock>
+      <ColorBlock backgroundColor='green-light' textColor='white'>
+        Light
+      </ColorBlock>
+      <ColorBlock backgroundColor='green-dark' textColor='white'>
+        Dark
+      </ColorBlock>
+      <ColorBlock backgroundColor='green-darker' textColor='white'>
+        Darker
+      </ColorBlock>
+    </ColorGroup>
 
-    <div className='md:flex-1 w-full p-12'>
-      <div className='m-auto max-w-xs'>
-        <h4 className='text-grey text-sm font-semibold mb-4'>Accent Yellow</h4>
-        <div className='bg-yellow text-black text-center rounded py-4 mb-4'>
-          Base
-        </div>
-        <div className='bg-yellow-light text-black text-center rounded py-4 mb-4'>
-          Light
-        </div>
-        <div className='bg-yellow-dark text-black text-center rounded py-4 mb-4'>
-          Dark
-        </div>
-        <div className='bg-yellow-darker text-black text-center rounded py-4'>
-          Darker
-        </div>
-      </div>
-    </div>
+    <ColorGroup>
+      <ColorName>Accent Yellow</ColorName>
+      <ColorBlock backgroundColor='yellow' textColor='black'>
+        Base
+      </ColorBlock>
+      <ColorBlock backgroundColor='yellow-light' textColor='black'>
+        Light
+      </ColorBlock>
+      <ColorBlock backgroundColor='yellow-dark' textColor='black'>
+        Dark
+      </ColorBlock>
+      <ColorBlock backgroundColor='yellow-darker' textColor='black'>
+        Darker
+      </ColorBlock>
+    </ColorGroup>
 
-    <div className='md:flex-1 w-full p-12'>
-      <div className='m-auto max-w-xs'>
-        <h4 className='text-grey text-sm font-semibold mb-4'>Accesible Blue</h4>
-        <div className='bg-blue text-white text-center rounded py-4 mb-4'>
-          Base
-        </div>
-        <div className='bg-blue-light text-white text-center rounded py-4 mb-4'>
-          Light
-        </div>
-        <div className='bg-blue-dark text-white text-center rounded py-4 mb-4'>
-          Dark
-        </div>
-        <div className='bg-blue-darker text-white text-center rounded py-4'>
-          Darker
-        </div>
-      </div>
-    </div>
+    <ColorGroup>
+      <ColorName>Accessible Blue</ColorName>
+      <ColorBlock backgroundColor='blue' textColor='white'>
+        Base
+      </ColorBlock>
+      <ColorBlock backgroundColor='blue-light' textColor='white'>
+        Light
+      </ColorBlock>
+      <ColorBlock backgroundColor='blue-dark' textColor='white'>
+        Dark
+      </ColorBlock>
+      <ColorBlock backgroundColor='blue-darker' textColor='white'>
+        Darker
+      </ColorBlock>
+    </ColorGroup>
 
-    <div className='md:flex-1 w-full p-12'>
-      <div className='m-auto max-w-xs'>
-        <h4 className='text-grey text-sm font-semibold mb-4'>BW & Greyscale</h4>
-        <div className='bg-black text-white text-center rounded py-4 mb-4'>
-          Black
-        </div>
-        <div className='bg-grey text-white text-center rounded py-4 mb-4'>
-          Grey
-        </div>
-        <div className='bg-grey-80 text-white text-center rounded py-4 mb-4'>
-          80%
-        </div>
-        <div className='bg-grey-40 text-black text-center rounded py-4 mb-4'>
-          40%
-        </div>
-        <div className='bg-grey-30 text-black text-center rounded py-4 mb-4'>
-          30%
-        </div>
-        <div className='bg-grey-20 text-black text-center rounded py-4 mb-4'>
-          20%
-        </div>
-        <div className='bg-grey-15 text-black text-center rounded py-4 mb-4'>
-          15%
-        </div>
-        <div className='bg-white text-black text-center rounded py-4 border border-grey-25'>
-          White
-        </div>
-      </div>
-    </div>
+    <ColorGroup>
+      <ColorName>BW & Greyscale</ColorName>
+      <ColorBlock backgroundColor='black' textColor='white'>
+        Black
+      </ColorBlock>
+      <ColorBlock backgroundColor='grey' textColor='white'>
+        Grey
+      </ColorBlock>
+      <ColorBlock backgroundColor='grey-80' textColor='white'>
+        80%
+      </ColorBlock>
+      <ColorBlock backgroundColor='grey-40' textColor='grey'>
+        40%
+      </ColorBlock>
+      <ColorBlock backgroundColor='grey-30' textColor='grey'>
+        30%
+      </ColorBlock>
+      <ColorBlock backgroundColor='grey-20' textColor='grey'>
+        20%
+      </ColorBlock>
+      <ColorBlock backgroundColor='grey-15' textColor='grey'>
+        15%
+      </ColorBlock>
+      <ColorBlock
+        backgroundColor='white'
+        textColor='grey'
+        className='border border-grey-40'
+      >
+        White
+      </ColorBlock>
+    </ColorGroup>
 
-    <div className='md:flex-1 w-full p-12'>
-      <div className='m-auto max-w-xs'>
-        <h4 className='text-grey text-sm font-semibold mb-4'>Success Green</h4>
-        <div className='bg-success text-white text-center rounded py-4 mb-4'>
-          Base
-        </div>
-        <div className='bg-success-light text-white text-center rounded py-4 mb-4'>
-          Light
-        </div>
-        <div className='bg-success-dark text-white text-center rounded py-4 mb-4'>
-          Dark
-        </div>
-        <div className='bg-success-darker text-white text-center rounded py-4'>
-          Darker
-        </div>
-      </div>
-    </div>
+    <ColorGroup>
+      <ColorName>Success Green</ColorName>
+      <ColorBlock backgroundColor='success' textColor='white'>
+        Base
+      </ColorBlock>
+      <ColorBlock backgroundColor='success-light' textColor='white'>
+        Light
+      </ColorBlock>
+      <ColorBlock backgroundColor='success-dark' textColor='white'>
+        Dark
+      </ColorBlock>
+      <ColorBlock backgroundColor='success-darker' textColor='white'>
+        Darker
+      </ColorBlock>
+    </ColorGroup>
 
-    <div className='md:flex-1 w-full p-12'>
-      <div className='m-auto max-w-xs'>
-        <h4 className='text-grey text-sm font-semibold mb-4'>Warning Yellow</h4>
-        <div className='bg-warning text-white text-center rounded py-4 mb-4'>
-          Base
-        </div>
-        <div className='bg-warning-light text-white text-center rounded py-4 mb-4'>
-          Light
-        </div>
-        <div className='bg-warning-dark text-white text-center rounded py-4 mb-4'>
-          Dark
-        </div>
-        <div className='bg-warning-darker text-white text-center rounded py-4'>
-          Darker
-        </div>
-      </div>
-    </div>
+    <ColorGroup>
+      <ColorName>Warning Yellow</ColorName>
+      <ColorBlock backgroundColor='warning' textColor='black'>
+        Base
+      </ColorBlock>
+      <ColorBlock backgroundColor='warning-light' textColor='black'>
+        Light
+      </ColorBlock>
+      <ColorBlock backgroundColor='warning-dark' textColor='black'>
+        Dark
+      </ColorBlock>
+      <ColorBlock backgroundColor='warning-darker' textColor='black'>
+        Darker
+      </ColorBlock>
+    </ColorGroup>
 
-    <div className='md:flex-1 w-full p-12'>
-      <div className='m-auto max-w-xs'>
-        <h4 className='text-grey text-sm font-semibold mb-4'>Error Red</h4>
-        <div className='bg-error text-white text-center rounded py-4 mb-4'>
-          Base
-        </div>
-        <div className='bg-error-light text-white text-center rounded py-4 mb-4'>
-          Light
-        </div>
-        <div className='bg-error-dark text-white text-center rounded py-4 mb-4'>
-          Dark
-        </div>
-        <div className='bg-error-darker text-white text-center rounded py-4'>
-          Darker
-        </div>
-      </div>
-    </div>
+    <ColorGroup>
+      <ColorName>Error Red</ColorName>
+      <ColorBlock backgroundColor='error' textColor='white'>
+        Base
+      </ColorBlock>
+      <ColorBlock backgroundColor='error-light' textColor='white'>
+        Light
+      </ColorBlock>
+      <ColorBlock backgroundColor='error-dark' textColor='white'>
+        Dark
+      </ColorBlock>
+      <ColorBlock backgroundColor='error-darker' textColor='white'>
+        Darker
+      </ColorBlock>
+    </ColorGroup>
   </div>
 )
