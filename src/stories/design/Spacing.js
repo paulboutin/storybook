5df24dd8a44f84c0aaef8ab4b888d8517e50c9b7
Sergio del Puerto <sergio.delpuerto@intelygenz.com>
2020-06-30 19:17:18 -0700

    Update the spacing story to dynamically use the config file

--- src/stories/design/Spacing.js
+++ src/stories/design/Spacing.js
@@ -1,117 +1,38 @@
 import React from 'react'
+import config from '../../../tailwind.config.js'
 
-export const Spacing = () => (
-  <div className='container'>
-    <h2 className='mb-12 text-grey-40'>Spacing scale</h2>
-    <div className='flex flex-wrap w-full justify-between items-end mb-48'>
-      <div className='flex flex-col items-center'>
-        <span className='block w-4 h-4 bg-green mb-12' />
-        <span>4</span>
+export const Spacing = () => {
+  const sizes = Object.keys(config.theme.spacing).slice(1)
+
+  return (
+    <div>
+      <h2 className='mb-12 text-grey-40'>Spacing scale</h2>
+      <div className='flex flex-wrap justify-between items-end mb-48'>
+        {sizes.map(size => (
+          <div key={size} className='flex flex-col items-center'>
+            <div className={`w-${size} h-${size} bg-green mb-12`} />
+            <span>{size}</span>
+          </div>
+        ))}
       </div>
-      <div className='flex flex-col items-center'>
-        <span className='block w-8 h-8 bg-green mb-12' />
-        <span>8</span>
+
+      <h2 className='mb-12 text-grey-40'>Padding</h2>
+      <div className='flex flex-wrap justify-between items-end mb-48'>
+        {sizes.slice(0, -1).map(size => (
+          <div key={size} className={`p-${size} bg-green mb-12`}>
+            <div className='w-16 h-16 bg-green-darker' />
+          </div>
+        ))}
       </div>
-      <div className='flex flex-col items-center'>
-        <span className='block w-12 h-12 bg-green mb-12' />
-        <span>12</span>
-      </div>
-      <div className='flex flex-col items-center'>
-        <span className='block w-16 h-16 bg-green mb-12' />
-        <span>16</span>
-      </div>
-      <div className='flex flex-col items-center'>
-        <span className='block w-24 h-24 bg-green mb-12' />
-        <span>24</span>
-      </div>
-      <div className='flex flex-col items-center'>
-        <span className='block w-32 h-32 bg-green mb-12' />
-        <span>32</span>
-      </div>
-      <div className='flex flex-col items-center'>
-        <span className='block w-48 h-48 bg-green mb-12' />
-        <span>48</span>
-      </div>
-      <div className='flex flex-col items-center'>
-        <span className='block w-64 h-64 bg-green mb-12' />
-        <span>64</span>
-      </div>
-      <div className='flex flex-col items-center'>
-        <span className='block w-96 h-96 bg-green mb-12' />
-        <span>96</span>
-      </div>
-      <div className='flex flex-col items-center'>
-        <span className='block w-160 h-160 bg-green mb-12' />
-        <span>160</span>
+
+      <h2 className='mb-12 text-grey-40'>Margin</h2>
+      <div>
+        {sizes.concat('0').map(size => (
+          <p key={size} className={`mb-${size} bg-grey-20`}>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+          </p>
+        ))}
       </div>
     </div>
-
-    <h2 className='mb-12 text-grey-40'>Padding</h2>
-    <div className='flex flex-wrap w-full justify-between items-end mb-48'>
-      <div className='bg-green p-4 mb-12'>
-        <span className='block w-16 h-16 bg-green-darker' />
-      </div>
-      <div className='bg-green p-8 mb-12'>
-        <span className='block w-16 h-16 bg-green-darker' />
-      </div>
-      <div className='bg-green p-12 mb-12'>
-        <span className='block w-16 h-16 bg-green-darker' />
-      </div>
-      <div className='bg-green p-16 mb-12'>
-        <span className='block w-16 h-16 bg-green-darker' />
-      </div>
-      <div className='bg-green p-24 mb-12'>
-        <span className='block w-16 h-16 bg-green-darker' />
-      </div>
-      <div className='bg-green p-32 mb-12'>
-        <span className='block w-16 h-16 bg-green-darker' />
-      </div>
-      <div className='bg-green p-48 mb-12'>
-        <span className='block w-16 h-16 bg-green-darker' />
-      </div>
-      <div className='bg-green p-64 mb-12'>
-        <span className='block w-16 h-16 bg-green-darker' />
-      </div>
-      <div className='bg-green p-96 mb-12'>
-        <span className='block w-16 h-16 bg-green-darker' />
-      </div>
-    </div>
-
-    <h2 className='mb-12 text-grey-40'>Margin</h2>
-    <div className='w-full'>
-      <p className='mb-4 bg-grey-20'>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-      </p>
-      <p className='mb-8 bg-grey-20'>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-      </p>
-      <p className='mb-12 bg-grey-20'>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-      </p>
-      <p className='mb-16 bg-grey-20'>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-      </p>
-      <p className='mb-24 bg-grey-20'>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-      </p>
-      <p className='mb-32 bg-grey-20'>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-      </p>
-      <p className='mb-48 bg-grey-20'>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-      </p>
-      <p className='mb-64 bg-grey-20'>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-      </p>
-      <p className='mb-96 bg-grey-20'>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-      </p>
-      <p className='mb-160 bg-grey-20'>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-      </p>
-      <p className='bg-grey-20'>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-      </p>
-    </div>
-  </div>
-)
+  )
+}
