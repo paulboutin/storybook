bc09bb217349d417541d035e7fcacd366675728f
Daniel Clowes <daniel.clowes@intelygenz.com>
2020-07-06 16:41:27 +0200

    create eyebrow base component

---
+++ src/stories/components/base/Eyebrow.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import classNames from 'classnames'
+import { text, radios } from '@storybook/addon-knobs'
+
+const defaultText = 'Mobile banking'
+
+export const Eyebrow = ({ text, alt = false }) => (
+  <div className={classNames('eyebrow', alt ? 'alt' : '')}>
+    <p className='eyebrow-text'>{text}</p>
+    {!alt && <div className='eyebrow-spacer' />}
+  </div>
+)
+
+export const EyebrowStory = () => {
+  const placeholder = text('Text', defaultText)
+  const type = radios('Type', ['Standard', 'Alt'], 'Standard')
+
+  return <Eyebrow text={placeholder} alt={type === 'Alt'} />
+}
+
+EyebrowStory.story = {
+  name: 'Eyebrow'
+}
