18c82bbfc07e45a4a3d14f36bb65dc06cfc3db8d
Daniel Clowes <daniel.clowes@intelygenz.com>
2020-07-06 17:08:57 +0200

    update link base component

--- src/stories/components/base/Link.js
+++ src/stories/components/base/Link.js
@@ -1,7 +1,18 @@
 import React from 'react'
 
-export const Link = () => (
-  <a href='#' className='link'>
-    Link
+export const Link = ({ href, children }) => (
+  <a href={href} className='link'>
+    {children}
   </a>
 )
+
+export const LinkStory = () => (
+  <Link href='#'>
+    Link
+    <span className='ml-8 font-awesome font-bold fa-long-arrow-alt-right' />
+  </Link>
+)
+
+LinkStory.story = {
+  name: 'Link'
+}
