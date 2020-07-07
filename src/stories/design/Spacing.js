b13bd724da453cc4ca14b4b42c16bda3f49ad627
Sergio del Puerto <sergio.delpuerto@intelygenz.com>
2020-07-06 21:17:07 -0700

    Refactor the design system spacing story to use SASS instead of Tailwind

--- src/stories/design/Spacing.js
+++ src/stories/design/Spacing.js
@@ -1,67 +1,67 @@
 import React from 'react'
 import classNames from 'classnames'
 
-const SpacingBlock = ({ children, className }) => (
-  <div className='flex flex-col items-center'>
-    <div className={classNames('bg-green mb-12', className)} />
-    <span>{children}</span>
+const SpacingBlock = ({ size }) => (
+  <div className='ds:spacing'>
+    <div className={classNames('ds:spacing-block', `ds:spacing--${size}`)} />
+    <span>{size}</span>
   </div>
 )
 
-const PaddingBlock = ({ className }) => (
-  <div className={classNames('bg-green mb-12', className)}>
-    <div className='w-16 h-16 bg-green-darker' />
+const PaddingBlock = ({ size }) => (
+  <div className={classNames('ds:spacing-block', `ds:padding--${size}`)}>
+    <div className='ds:spacing-block--padding' />
   </div>
 )
 
-const MarginBlock = ({ className }) => (
-  <p className={classNames('bg-grey-20', className)}>
+const MarginBlock = ({ size }) => (
+  <p className={classNames('ds:spacing-block--margin', `ds:margin--${size}`)}>
     Lorem ipsum dolor sit amet, consectetur adipisicing elit.
   </p>
 )
 
 export const Spacing = () => (
   <div>
-    <h2 className='mb-12 text-grey-40'>Spacing scale</h2>
-    <div className='flex flex-wrap justify-between items-end mb-48'>
-      <SpacingBlock className='w-4 h-4'>4</SpacingBlock>
-      <SpacingBlock className='w-8 h-8'>8</SpacingBlock>
-      <SpacingBlock className='w-12 h-12'>12</SpacingBlock>
-      <SpacingBlock className='w-16 h-16'>16</SpacingBlock>
-      <SpacingBlock className='w-24 h-24'>24</SpacingBlock>
-      <SpacingBlock className='w-32 h-32'>32</SpacingBlock>
-      <SpacingBlock className='w-48 h-48'>48</SpacingBlock>
-      <SpacingBlock className='w-64 h-64'>64</SpacingBlock>
-      <SpacingBlock className='w-96 h-96'>96</SpacingBlock>
-      <SpacingBlock className='w-160 h-160'>160</SpacingBlock>
+    <h2 className='ds:spacing-heading'>Spacing scale</h2>
+    <div className='ds:spacing-block-section'>
+      <SpacingBlock size={4} />
+      <SpacingBlock size={8} />
+      <SpacingBlock size={12} />
+      <SpacingBlock size={16} />
+      <SpacingBlock size={24} />
+      <SpacingBlock size={32} />
+      <SpacingBlock size={48} />
+      <SpacingBlock size={64} />
+      <SpacingBlock size={96} />
+      <SpacingBlock size={160} />
     </div>
 
-    <h2 className='mb-12 text-grey-40'>Padding</h2>
-    <div className='flex flex-wrap justify-between items-end mb-48'>
-      <PaddingBlock className='p-4' />
-      <PaddingBlock className='p-8' />
-      <PaddingBlock className='p-12' />
-      <PaddingBlock className='p-16' />
-      <PaddingBlock className='p-24' />
-      <PaddingBlock className='p-32' />
-      <PaddingBlock className='p-48' />
-      <PaddingBlock className='p-64' />
-      <PaddingBlock className='p-96' />
+    <h2 className='ds:spacing-heading'>Padding</h2>
+    <div className='ds:spacing-block-section'>
+      <PaddingBlock size={4} />
+      <PaddingBlock size={8} />
+      <PaddingBlock size={12} />
+      <PaddingBlock size={16} />
+      <PaddingBlock size={24} />
+      <PaddingBlock size={32} />
+      <PaddingBlock size={48} />
+      <PaddingBlock size={64} />
+      <PaddingBlock size={96} />
     </div>
 
-    <h2 className='mb-12 text-grey-40'>Margin</h2>
+    <h2 className='ds:spacing-heading'>Margin</h2>
     <div>
-      <MarginBlock className='mt-0' />
-      <MarginBlock className='mt-4' />
-      <MarginBlock className='mt-8' />
-      <MarginBlock className='mt-12' />
-      <MarginBlock className='mt-16' />
-      <MarginBlock className='mt-24' />
-      <MarginBlock className='mt-32' />
-      <MarginBlock className='mt-48' />
-      <MarginBlock className='mt-64' />
-      <MarginBlock className='mt-96' />
-      <MarginBlock className='mt-160' />
+      <MarginBlock size={0} />
+      <MarginBlock size={4} />
+      <MarginBlock size={8} />
+      <MarginBlock size={12} />
+      <MarginBlock size={16} />
+      <MarginBlock size={24} />
+      <MarginBlock size={32} />
+      <MarginBlock size={48} />
+      <MarginBlock size={64} />
+      <MarginBlock size={96} />
+      <MarginBlock size={160} />
     </div>
   </div>
 )
