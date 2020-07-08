3e88fcb7092cd529d20fb81e2008f9c899ee7caa
Sergio del Puerto <sergio.delpuerto@intelygenz.com>
2020-07-08 14:58:42 -0700

    Use a custom snapshot name generator to greatly shorten the filenames

--- test/integration/setup.js
+++ test/integration/setup.js
@@ -3,6 +3,12 @@ const { configureToMatchImageSnapshot } = require('jest-image-snapshot')
 const toMatchImageSnapshot = configureToMatchImageSnapshot({
   customDiffConfig: {
     threshold: 0
+  },
+  customSnapshotIdentifier: ({ currentTestName }) => {
+    return currentTestName
+      .replace(/\s/g, '-')
+      .toLowerCase()
+      .replace('matches-snapshot-on-', '')
   }
 })
 
