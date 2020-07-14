f84d850233f5bf72e1e617eab6005b6fdc1c51aa
Sergio del Puerto <sergio.delpuerto@intelygenz.com>
2020-07-13 17:26:58 -0700

    Crete Column Icons component

---
+++ src/stories/components/layout/ColumnIcons.js
@@ -0,0 +1,40 @@
+import React from 'react'
+
+const Column = ({ text, subtext }) => (
+  <div className='column-icon'>
+    <div className='column-icon-circle'>
+      <img
+        className='column-icon-img'
+        src='http://via.placeholder.com/256'
+        alt='Icon description'
+      />
+    </div>
+
+    <h4>{text}</h4>
+    {subtext && <p className='column-icon-subtext'>{subtext}</p>}
+  </div>
+)
+
+export const ColumnIcons = () => (
+  <div className='column-icons'>
+    <div className='column-icons-section'>
+      <Column
+        text='Debit card? Yes!'
+        subtext='Use it anywhere that accepts a Mastercard debit card.'
+      />
+      <Column
+        text='Overdraft protection? Yes!'
+        subtext='We can all lose track of our spending. Check out our overdraft programs.'
+      />
+      <Column
+        text='Mobile wallet? Yes!'
+        subtext='Use it anywhere that accepts a Mastercard debit card.'
+      />
+    </div>
+
+    <div className='column-icons-section'>
+      <Column text='$0 monthly fee with any deposit per statement' />
+      <Column text='No minimum balance required to maintain your account' />
+    </div>
+  </div>
+)
