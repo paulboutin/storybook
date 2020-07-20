e6fb3c5ea0c1f49e49796667984a218180b354bc
Sergio del Puerto <sergio.delpuerto@intelygenz.com>
2020-07-20 15:53:46 -0700

    Refactor the Column Icons component to use the new Image component

--- src/components/layout/ColumnIcons.js
+++ src/components/layout/ColumnIcons.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import Image from '../base/Image'
 import classNames from 'classnames'
 
 const Column = ({ image, text, subtext }) => (
   <div className='column-icon'>
     <div className='column-icon-circle'>
-      <img src={image} alt='Icon description' className='column-icon-img' />
+      <Image src={image} alt='Icon description' ratio='1:1' />
     </div>
 
     <h4 className='column-icon-text'>{text}</h4>
