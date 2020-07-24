179c0a310f5301e5d10f515b2180bc69fad8b738
Sergio del Puerto <sergio.delpuerto@intelygenz.com>
2020-07-24 15:59:53 -0700

    Minor improvement to the Image component

--- src/components/base/Image.js
+++ src/components/base/Image.js
@@ -1,13 +1,10 @@
 import React from 'react'
 import classNames from 'classnames'
 
-const Image = ({ src, alt, ratio, className }) =>
-  ratio === 'Auto' ? (
-    <img src={src} alt={alt} className={className} />
-  ) : (
-    <div className={classNames(`ratio-${ratio}`, className)}>
-      <img src={src} alt={alt} />
-    </div>
-  )
+const Image = ({ src, alt, ratio = 'auto', className }) => (
+  <div className={classNames(`ratio-${ratio}`, className)}>
+    <img src={src} alt={alt} />
+  </div>
+)
 
 export default Image
