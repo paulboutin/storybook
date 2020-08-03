3af4158995dbba4a7475180bbcc95645a9f3010f
Sergio del Puerto <sergio.delpuerto@intelygenz.com>
2020-08-03 15:44:19 -0700

    Remove the InVision DSM integration since it doesn't work

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,6 @@
 import { addDecorator, addParameters, configure } from '@storybook/html'
 import { withKnobs } from '@storybook/addon-knobs'
 import { withHTML } from '@whitespace/storybook-addon-html/html'
-import { initDsm } from '@invisionapp/dsm-storybook'
 import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport'
 import renderHTML from '../src/decorators/renderHTML'
 import toggleBodyClass from '../src/decorators/toggleBodyClass'
@@ -28,13 +27,5 @@ const customViewports = {
 addParameters({
   viewport: {
     viewports: { ...INITIAL_VIEWPORTS, ...customViewports }
-  }
-})
-
-initDsm({
-  addDecorator,
-  addParameters,
-  callback: () => {
-    configure(require.context('../src/stories', true, /\.stories\.js$/), module)
   }
 })
