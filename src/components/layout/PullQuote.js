a8f1a01e2b03fe7922df7a7f5402433088c907a3
Sergio del Puerto <sergio.delpuerto@intelygenz.com>
2020-08-04 15:28:42 -0700

    [WIP] Refactor the PullQuote component to take an array of items

--- src/components/layout/PullQuote.js
+++ src/components/layout/PullQuote.js
@@ -3,27 +3,26 @@ import Eyebrow from '../base/Eyebrow'
 import ImageOverlap from '../base/ImageOverlap'
 import Button from '../base/Button'
 
-const PullQuote = ({ image }) => (
+const PullQuote = ({ items }) => (
   <div className='pull-quote container'>
-    <Eyebrow text='Our members' />
-    <h2 className='pull-quote-title'>Testimonials From Our Team</h2>
+    <Eyebrow text='Bank of the West' />
+    <h2 className='pull-quote-title'>From Our Team</h2>
 
     <div className='pull-quote-outer-wrapper'>
-      <ImageOverlap images={[image]} position='right' color='primary' />
+      <ImageOverlap
+        images={[items[0].image]}
+        position='right'
+        color='primary'
+      />
 
       <div className='pull-quote-middle-wrapper'>
         <i className='icon icon-quote' />
 
         <div className='pull-quote-inner-wrapper'>
-          <blockquote>
-            Life isn’t stopping, neither are we. We understand the important
-            role that we play in people’s lives.
-          </blockquote>
+          <blockquote>{items[0].quote}</blockquote>
 
-          <h6 className='pull-quote-author'>—Nandita Bakhshi</h6>
-          <p className='pull-quote-description'>
-            President and CEO at Bank of the West
-          </p>
+          <h6 className='pull-quote-author'>—{items[0].author.name}</h6>
+          <p className='pull-quote-description'>{items[0].author.role}</p>
 
           <div className='pull-quote-buttons'>
             <Button type='secondary' icon='chevron-left' />
