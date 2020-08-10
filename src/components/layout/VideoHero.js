c73cea7cfd5cfbbf3b74bd953e63a55bdb8c9f89
Sergio del Puerto <sergio.delpuerto@intelygenz.com>
2020-08-10 12:46:47 -0700

    Keep VideoHero component at a fixed 16:9 aspect ratio on bigger screens

--- src/components/layout/VideoHero.js
+++ src/components/layout/VideoHero.js
@@ -4,18 +4,20 @@ import Button from '../base/Button'
 const VideoHero = ({ video, poster }) => {
   return (
     <div className='video-hero'>
-      <video src={video} poster={poster} autoPlay loop muted playsInline />
-      <div className='video-hero-overlay' />
+      <div className='video-hero-wrapper'>
+        <video src={video} poster={poster} autoPlay loop muted playsInline />
+        <div className='video-hero-overlay' />
 
-      <div className='video-hero-content container'>
-        <h1>The first bank account designed for climate action</h1>
+        <div className='video-hero-content container'>
+          <h1>The first bank account designed for climate action</h1>
 
-        <p className='video-hero-text'>
-          1% of our net revenue on this account will go to Protect Our Winters,
-          a non-profit partner
-        </p>
+          <p className='video-hero-text'>
+            1% of our net revenue on this account will go to Protect Our
+            Winters, a non-profit partner
+          </p>
 
-        <Button type='tertiary' label='Learn more' />
+          <Button type='tertiary' label='Learn more' />
+        </div>
       </div>
     </div>
   )
