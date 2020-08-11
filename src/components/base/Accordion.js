5897ec8f5fc1e1f20b9bbe16e841d4880eb41cf2
Sergio del Puerto <sergio.delpuerto@intelygenz.com>
2020-08-11 11:46:03 -0700

    Add inverted variant to the Accordion component

--- src/components/base/Accordion.js
+++ src/components/base/Accordion.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import classNames from 'classnames'
 
-const Accordion = ({ title, children, expanded, className }) => (
+const Accordion = ({ title, children, expanded, inverted, className }) => (
   <div
     className={classNames(
       'accordion',
       { 'accordion-expanded': expanded },
+      { 'accordion-inverted': inverted },
       className
     )}
   >
