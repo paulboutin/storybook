282b2410a292d34e14ebd8a7dcb5acf634368823
Sergio del Puerto <sergio.delpuerto@intelygenz.com>
2020-08-18 12:22:53 -0700

    Keep DropdownHero component at a fixed 16:9 aspect ratio on bigger screens

--- src/components/layout/DropdownHero.js
+++ src/components/layout/DropdownHero.js
@@ -1,24 +1,20 @@
 import React from 'react'
 import Select from '../base/Select'
 
-const DropdownHero = ({ image, items }) => {
-  return (
-    <div className='dropdown-hero'>
-      <img src={image} alt='Hero image' className='dropdown-hero-img' />
+const DropdownHero = ({ image, items }) => (
+  <div className='dropdown-hero'>
+    <img src={image} alt='Hero image' className='dropdown-hero-img' />
 
-      <div className='dropdown-hero-content container'>
-        <h1 className='dropdown-hero-title'>
-          We stand with your small business
-        </h1>
-        <Select
-          className='dropdown-hero-select'
-          type='links'
-          label='How can we help you?'
-          items={items}
-        />
-      </div>
+    <div className='dropdown-hero-content container'>
+      <h1 className='dropdown-hero-title'>We stand with your small business</h1>
+      <Select
+        className='dropdown-hero-select'
+        type='links'
+        label='How can we help you?'
+        items={items}
+      />
     </div>
-  )
-}
+  </div>
+)
 
 export default DropdownHero
