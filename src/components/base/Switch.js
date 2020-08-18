395b43031bc66879de83d3405efd08ba1d2896c2
Sergio del Puerto <sergio.delpuerto@intelygenz.com>
2020-08-18 12:34:50 -0700

    Update Switch component styles

--- src/components/base/Switch.js
+++ src/components/base/Switch.js
@@ -4,12 +4,11 @@ import classNames from 'classnames'
 const Switch = ({ className, label, checked, disabled }) => (
   <label className={classNames('switch-wrapper', className)}>
     <input type='checkbox' defaultChecked={checked} disabled={disabled} />
+    <span className='switch-label'>{label}</span>
 
     <div className='switch'>
       <div className='switch-circle' />
     </div>
-
-    <span className='switch-label'>{label}</span>
   </label>
 )
 
