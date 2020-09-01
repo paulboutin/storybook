3b2323715a08bf0e20b2a1f21dd85b3cc6ee235e
Sergio del Puerto <sergio.delpuerto@intelygenz.com>
2020-08-31 19:37:06 -0700

    [WIP] Create ComparisonTable component

---
+++ src/components/layout/ComparisonTable.js
@@ -0,0 +1,65 @@
+import React from 'react'
+import Button from '../base/Button'
+import Link from '../base/Link'
+import { ListElementItems } from './ListElement'
+
+const ComparisonTable = () => (
+  <div className='comparison-table'>
+    <div className='comparison-table-column'>
+      <h4 className='comparison-table-title'>Any Deposit Checking</h4>
+      <p>An account that makes it easy to waive the monthly service charge</p>
+      <Button type='primary' label='Apply Now' />
+      <div className='comparison-table-divider' />
+
+      <ListElementItems
+        items={[
+          '$0 monthly service charge with any deposit per statement',
+          'No minimum balance',
+          'No direct deposit required'
+        ]}
+      />
+
+      <Link standalone>View account details</Link>
+    </div>
+
+    <div className='comparison-table-column'>
+      <h4 className='comparison-table-title'>1% for the Planet</h4>
+      <p>An account that helps the planet (at no cost to you)</p>
+      <Button type='primary' label='Apply Now' />
+      <div className='comparison-table-divider' />
+      <p className='comparison-table-extra'>
+        All the benefits of our Any Deposit Checking account, plus:
+      </p>
+
+      <ListElementItems
+        items={[
+          "We'll donate 1% of our revenue on this account to 1% for the Planet—at no cost to you",
+          'CO2 tracking for your debit card purchases, right in the app',
+          'Compostable debit card'
+        ]}
+      />
+
+      <Link standalone>View account details</Link>
+    </div>
+
+    <div className='comparison-table-column'>
+      <h4 className='comparison-table-title'>Premier Checking</h4>
+      <p>An account with the highest level of service and travel benefits</p>
+      <Button type='primary' label='Apply Now' />
+      <div className='comparison-table-divider' />
+
+      <ListElementItems
+        items={[
+          'No fees on checks, money orders, incoming wires, or ATM withdrawals worldwide',
+          'Relationship-based service and special rates on CDs and Money Market accounts',
+          'Save on consumer loans with interest rate discounts',
+          '$0 monthly service charge with minimum balance of $25,000'
+        ]}
+      />
+
+      <Link standalone>View account details</Link>
+    </div>
+  </div>
+)
+
+export default ComparisonTable
---
+++ src/stories/components/layout/ComparisonTable.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ComparisonTable from '../../../components/layout/ComparisonTable'
+
+export const ComparisonTableStory = () => {
+  return <ComparisonTable />
+}
+
+ComparisonTableStory.story = {
+  name: 'Comparison Table'
+}
