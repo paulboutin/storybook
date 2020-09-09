df97edc833673c6de51cab92308ab76943f69f7b
Sergio del Puerto <sergio.delpuerto@intelygenz.com>
2020-09-09 15:55:57 -0700

    Update zip build process to include public assets into a single zip instead of a zip file per folder

--- src/stories/design/Download.js
+++ src/stories/design/Download.js
@@ -1,14 +1,4 @@
 import React from 'react'
 import Link from '../../components/base/Link'
 
-export const Download = () => (
-  <div>
-    <div>
-      <Link href='/styles.zip'>Styles</Link>
-    </div>
-
-    <div>
-      <Link href='/scripts.zip'>Scripts</Link>
-    </div>
-  </div>
-)
+export const Download = () => <Link href='/lib.zip'>Download</Link>
