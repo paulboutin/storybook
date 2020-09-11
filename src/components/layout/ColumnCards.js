3d8d1e4448919c18b052dcc5b898f045a171876e
Sergio del Puerto <sergio.delpuerto@intelygenz.com>
2020-09-10 19:06:56 -0700

    Update old class names

--- src/components/layout/ColumnCards.js
+++ src/components/layout/ColumnCards.js
@@ -2,9 +2,13 @@ import classNames from 'classnames'
 import React from 'react'
 
 const ColumnCards = ({ heading, children, inverted }) => (
-  <div className={classNames('three-col', { 'three-col-inverted': inverted })}>
+  <div
+    className={classNames('column-cards', {
+      'column-cards-inverted': inverted
+    })}
+  >
     <div className='container'>
-      <h2 className='three-col-heading'>{heading}</h2>
+      <h2 className='column-cards-heading'>{heading}</h2>
       <div className='row'>
         {children.map((child, index) => (
           <div key={index} className='col-md-4'>
