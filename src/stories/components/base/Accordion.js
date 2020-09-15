4856def0a693005c932d73494188897534d80294
Sergio del Puerto <sergio.delpuerto@intelygenz.com>
2020-09-14 18:48:05 -0700

    Use different contents on the Accordions story to emphasize the fact that any HTML can be used

--- src/stories/components/base/Accordion.js
+++ src/stories/components/base/Accordion.js
@@ -2,16 +2,26 @@ import React from 'react'
 import Accordion from '../../../components/base/Accordion'
 import { number, text } from '@storybook/addon-knobs'
 
+const defaultContent = [
+  '<p>Accordion content</p>',
+  '<img src="http://via.placeholder.com/160">',
+  '<ul style="list-style: initial; padding-left: 40px;">' +
+    '\n  <li>List item 1</li>' +
+    '\n  <li>List item 2</li>' +
+    '\n  <li>List item 3</li>' +
+    '\n</ul>'
+]
+
 export const AccordionStory = () => {
   const accordions = []
   const accordionNumber = number('Accordions', 3)
 
-  for (let i = 1; i <= accordionNumber; i++) {
-    const prefix = `Accordion ${i} -`
+  for (let i = 0; i < accordionNumber; i++) {
+    const prefix = `Accordion ${i + 1} -`
 
     accordions.push({
-      title: text(`${prefix} Title`, `Accordion ${i} title`),
-      content: text(`${prefix} Content`, `<p>Accordion ${i} content</p>`)
+      title: text(`${prefix} Title`, `Accordion ${i + 1} title`),
+      content: text(`${prefix} Content`, defaultContent[i])
     })
   }
 
