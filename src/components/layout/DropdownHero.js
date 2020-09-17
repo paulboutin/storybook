89211c7272a25b4d0ac2cc77ba260355e259aef9
Sergio del Puerto <sergio.delpuerto@intelygenz.com>
2020-09-17 15:28:15 -0700

    Add overlay to the DropdownHero component

--- src/components/layout/DropdownHero.js
+++ src/components/layout/DropdownHero.js
@@ -4,6 +4,7 @@ import Select from '../base/Select'
 const DropdownHero = ({ image, items }) => (
   <div className='dropdown-hero'>
     <img src={image} alt='Hero image' className='dropdown-hero-img' />
+    <div className='dropdown-hero-overlay' />
 
     <div className='dropdown-hero-content container'>
       <h1 className='dropdown-hero-title'>We stand with your small business</h1>
