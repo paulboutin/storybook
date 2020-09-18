8814a057a2e940188e7e6afabe9b69a17538964a
Sergio del Puerto <sergio.delpuerto@intelygenz.com>
2020-09-18 12:10:39 -0700

    Move all stories files to the same folder

---
+++ src/stories/base-components.stories.js
@@ -0,0 +1,17 @@
+export default {
+  title: 'Components/Base'
+}
+
+export { AccordionStory } from './components/base/Accordion'
+export { BreadcrumbsStory } from './components/base/Breadcrumbs'
+export { ButtonStory } from './components/base/Button'
+export { CardStory } from './components/base/Card'
+export { CheckboxStory } from './components/base/Checkbox'
+export { EyebrowStory } from './components/base/Eyebrow'
+export { ImageStory } from './components/base/Image'
+export { InputStory } from './components/base/Input'
+export { LinkStory } from './components/base/Link'
+export { RadioStory } from './components/base/Radio'
+export { SelectStory } from './components/base/Select'
+export { SwitchStory } from './components/base/Switch'
+export { TableStory } from './components/base/Table'
---
+++ src/stories/templates.stories.js
@@ -0,0 +1,10 @@
+export default {
+  title: 'Templates'
+}
+
+export { AccountDisclosure } from './templates/AccountDisclosure'
+export { ComparisonPage } from './templates/ComparisonPage'
+export { InterestRates } from './templates/InterestRates'
+export { ProductPage } from './templates/ProductPage'
+export { RatesAndFees } from './templates/RatesAndFees'
+export { TermsOfUse } from './templates/TermsOfUse'
