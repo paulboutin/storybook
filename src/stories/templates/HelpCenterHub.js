4af671279e00e77fa15f64f876274d04cb346386
Sergio del Puerto <sergio.delpuerto@intelygenz.com>
2020-09-22 11:03:43 -0700

    Update HelpCenterHub page styles

--- src/stories/templates/HelpCenterHub.js
+++ src/stories/templates/HelpCenterHub.js
@@ -1,76 +1,55 @@
 import React from 'react'
 import Navigation from '../../components/layout/Navigation'
 import HelpIntro from '../../components/layout/HelpIntro'
+import FAQ from '../../components/layout/FAQ'
 import HelpOutro from '../../components/layout/HelpOutro'
 import Legal from '../../components/layout/Legal'
 import Footer from '../../components/layout/Footer'
+import icon from '../../icons/img/card-services.svg'
 
-export const HelpCenterHub = () => (
-  <>
-    <Navigation />
+export const HelpCenterHub = () => {
+  const topic = {
+    icon: 'card-services',
+    title: 'Accounts',
+    description: 'Find answers to frequently asked questions grouped by topic'
+  }
+  const topics = new Array(6).fill(topic)
 
-    <main className='help-center-hub'>
-      <HelpIntro showLinks />
+  return (
+    <>
+      <Navigation />
 
-      <section className='help-center-topics'>
-        <div className='container'>
-          <div className='help-center-topics-wrapper'>
-            <div className='help-center-topic-wrapper'>
-              <a href='#' className='help-center-topic'>
-                Accounts
-              </a>
-            </div>
+      <main className='help-center-hub'>
+        <HelpIntro showLinks />
 
-            <div className='help-center-topic-wrapper'>
-              <a href='#' className='help-center-topic'>
-                COVID Updates
-              </a>
-            </div>
-
-            <div className='help-center-topic-wrapper'>
-              <a href='#' className='help-center-topic'>
-                Resource Hub
-              </a>
-            </div>
-
-            <div className='help-center-topic-wrapper'>
-              <a href='#' className='help-center-topic'>
-                Online Banking
-              </a>
-            </div>
-
-            <div className='help-center-topic-wrapper'>
-              <a href='#' className='help-center-topic'>
-                Mobile Banking
-              </a>
-            </div>
-
-            <div className='help-center-topic-wrapper'>
-              <a href='#' className='help-center-topic'>
-                Disputes
-              </a>
-            </div>
-
-            <div className='help-center-topic-wrapper'>
-              <a href='#' className='help-center-topic'>
-                Video Tutorials
-              </a>
-            </div>
-
-            <div className='help-center-topic-wrapper'>
-              <a href='#' className='help-center-topic'>
-                Learning Center
-              </a>
+        <section className='help-center-topics'>
+          <div className='container'>
+            <div className='help-center-topics-wrapper'>
+              {topics.map((topic, index) => (
+                <div key={index} className='help-center-topic-wrapper'>
+                  <a href='#'>
+                    <article className='help-center-topic'>
+                      <div className='help-center-topic-circle'>
+                        <img src={icon} alt={topic.title} />
+                      </div>
+                      <h5 className='help-center-topic-title'>{topic.title}</h5>
+                      <p>{topic.description}</p>
+                    </article>
+                  </a>
+                </div>
+              ))}
             </div>
           </div>
-        </div>
-      </section>
+        </section>
 
-      <HelpOutro />
+        <FAQ />
 
-      <Legal />
+        <HelpOutro />
 
-      <Footer />
-    </main>
-  </>
-)
+        <Legal />
+
+        <Footer />
+      </main>
+    </>
+  )
+}
