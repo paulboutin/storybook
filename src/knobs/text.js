a38c777af7ccf2d0f6e3feef91fa25f247eac48e
Sergio del Puerto <sergio.delpuerto@intelygenz.com>
2020-09-24 11:39:10 -0700

    Move text helper from an individual file into the only place that's used

--- src/knobs/text.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { text } from '@storybook/addon-knobs'
-
-export default (...args) => (
-  <span
-    dangerouslySetInnerHTML={{ __html: text(...args).replace(/\n/g, '<br>') }}
-  />
-)
--- src/stories/_design-system/typography.story.js
+++ src/stories/_design-system/typography.story.js
@@ -1,10 +1,16 @@
 import React from 'react'
-import text from '../../knobs/text'
+import { text } from '@storybook/addon-knobs'
 
 export const Typography = () => {
-  const txt = text(
+  const rawText = text(
     'Text',
     'Where you put your money matters.\nA bank that finances a sustainable tomorrow.'
+  )
+
+  const htmlText = (
+    <span
+      dangerouslySetInnerHTML={{ __html: rawText.replace(/\n/g, '<br>') }}
+    />
   )
 
   return (
@@ -12,36 +18,36 @@ export const Typography = () => {
       <h2 className='sb:typography-heading'>Headings</h2>
 
       <p className='sb:typography-title'>Heading 1</p>
-      <h1 className='sb:typography-text'>{txt}</h1>
+      <h1 className='sb:typography-text'>{htmlText}</h1>
 
       <p className='sb:typography-title'>Heading 2</p>
-      <h2 className='sb:typography-text'>{txt}</h2>
+      <h2 className='sb:typography-text'>{htmlText}</h2>
 
       <p className='sb:typography-title'>Heading 3</p>
-      <h3 className='sb:typography-text'>{txt}</h3>
+      <h3 className='sb:typography-text'>{htmlText}</h3>
 
       <p className='sb:typography-title'>Heading 4</p>
-      <h4 className='sb:typography-text'>{txt}</h4>
+      <h4 className='sb:typography-text'>{htmlText}</h4>
 
       <p className='sb:typography-title'>Heading 5</p>
-      <h5 className='sb:typography-text'>{txt}</h5>
+      <h5 className='sb:typography-text'>{htmlText}</h5>
 
       <p className='sb:typography-title'>Heading 6</p>
-      <h6 className='sb:typography-text'>{txt}</h6>
+      <h6 className='sb:typography-text'>{htmlText}</h6>
 
       <h2 className='sb:typography-heading'>Body</h2>
 
       <p className='sb:typography-title'>Body large</p>
-      <div className='sb:typography-text--lg'>{txt}</div>
+      <div className='sb:typography-text--lg'>{htmlText}</div>
 
       <p className='sb:typography-title'>Body base</p>
-      <div className='sb:typography-text--base'>{txt}</div>
+      <div className='sb:typography-text--base'>{htmlText}</div>
 
       <p className='sb:typography-title'>Body small</p>
-      <div className='sb:typography-text--sm'>{txt}</div>
+      <div className='sb:typography-text--sm'>{htmlText}</div>
 
       <p className='sb:typography-title'>Body extra small</p>
-      <div className='sb:typography-text--xs'>{txt}</div>
+      <div className='sb:typography-text--xs'>{htmlText}</div>
     </div>
   )
 }
