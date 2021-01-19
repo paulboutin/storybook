7030d4bd3a9ed12cffb90b8dd001da096791222e
Daniel Clowes <daniel.clowes@intelygenz.com>
2021-01-19 15:28:22 +0100

    improve knobs for alerts story

--- src/stories/base-components/alert.story.js
+++ src/stories/base-components/alert.story.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { text, radios, boolean } from '@storybook/addon-knobs'
+import { text, radios, boolean, button } from '@storybook/addon-knobs'
 import Alert from '../../components/base/Alert'
 import ProductPage from '../templates/product-page'
 
@@ -14,15 +14,37 @@ const defaultTitle = 'Alert Title Placeholder'
 const defaultText =
   'Your current browser may not be able to run our content. To update to the latest version <a class="link" href="">click here</a>.'
 
+let alerts = [
+  <Alert
+    key={0}
+    type={options.Danger}
+    title={defaultTitle}
+    text={defaultText}
+  />
+]
+
 export const AlertStory = () => {
-  const title = text('Title', defaultTitle)
-  const textContent = text('Text', defaultText)
-  const type = radios('Type', options, options.Danger)
+  const title = text('Title', defaultTitle, 'new alert')
+  const textContent = text('Text', defaultText, 'new alert')
+  const type = radios('Type', options, options.Danger, 'new alert')
+
+  const handleAddAlert = () => {
+    const count = Array.from(document.querySelectorAll('.alert')).length
+
+    alerts = alerts.slice(0, count) // maintain current count
+
+    alerts.push(
+      <Alert key={alerts.length} type={type} title={title} text={textContent} />
+    )
+  }
+
+  button('Add New Alert', handleAddAlert, 'new alert')
+
   const withProductPage = boolean('Show Product Page', false)
 
   return (
     <>
-      <Alert type={type} title={title} text={textContent} />
+      {alerts}
       {withProductPage && <ProductPage />}
     </>
   )
