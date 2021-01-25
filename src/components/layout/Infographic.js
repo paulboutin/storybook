b0760531d584b9e407d68d3d69154fbfa0e7f101
Daniel Clowes <daniel.clowes@intelygenz.com>
2021-01-25 14:28:04 +0100

    WIP additional controls for infographic component

--- src/components/layout/Infographic.js
+++ src/components/layout/Infographic.js
@@ -1,8 +1,25 @@
 import React from 'react'
 import classNames from 'classnames'
 import Button from '../base/Button'
+import Image from '../base/Image'
 
-const InfographicBox = ({ title, children }) => (
+export const defaultItems = [
+  {
+    title: '962',
+    content:
+      'kilotonnes of CO2 (eq.) emissions avoided as a result of sustainable energy projects financed by BOTW.'
+  },
+  {
+    title: '2.9',
+    content: 'kilotonnes of CO2 (eq.) BOTW’s own emissions, 100% compensation.'
+  },
+  {
+    title: '30%',
+    content: 'of our leadership is composed of women, including our CEO.'
+  }
+]
+
+export const InfographicBox = ({ title, children }) => (
   <article className='infographic-box'>
     <div className='infographic-box-wrapper'>
       <h1 className='infographic-box-title'>{title}</h1>
@@ -11,25 +28,18 @@ const InfographicBox = ({ title, children }) => (
   </article>
 )
 
-const Infographic = ({ className }) => {
+export const InfographicBoxImage = ({ src }) => (
+  <article className='infographic-box'>
+    <Image src={src} />
+  </article>
+)
+
+export const Infographic = ({ className, children }) => {
   return (
     <section className={classNames('infographic', className)}>
       <h2 className='infographic-title'>We invest in what we believe</h2>
 
-      <div className='infographic-boxes container'>
-        <InfographicBox title='962'>
-          kilotonnes of CO2 (eq.) emissions avoided as a result of sustainable
-          energy projects financed by BOTW.
-        </InfographicBox>
-
-        <InfographicBox title='2.9'>
-          kilotonnes of CO2 (eq.) BOTW’s own emissions, 100% compensation.
-        </InfographicBox>
-
-        <InfographicBox title='30%'>
-          of our leadership is composed of women, including our CEO.
-        </InfographicBox>
-      </div>
+      <div className='infographic-boxes container'>{children}</div>
 
       <Button
         type='tertiary'
@@ -41,4 +51,14 @@ const Infographic = ({ className }) => {
   )
 }
 
-export default Infographic
+const DefaultInfographic = ({ className }) => {
+  return (
+    <Infographic className={className}>
+      {defaultItems.map(({ title, content }) => (
+        <InfographicBox title={title}>{content}</InfographicBox>
+      ))}
+    </Infographic>
+  )
+}
+
+export default DefaultInfographic
