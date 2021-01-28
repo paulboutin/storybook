cdc4975407db8132ba807063a4466613b2f6f3a9
Daniel Clowes <daniel.clowes@intelygenz.com>
2021-01-28 23:14:39 +0100

    update js script to support multiple popup video components

--- src/js/components/popup-video.js
+++ src/js/components/popup-video.js
@@ -1,21 +1,22 @@
 export default () => {
-  const popupVideo = document.querySelector('.popup-video')
-  if (!popupVideo) return
+  const popupVideos = document.querySelectorAll('.popup-video')
 
-  const play = popupVideo.querySelector('.button')
-  const close = popupVideo.querySelector('.icon-close')
-  const iframe = popupVideo.querySelector('iframe')
-  const player = new Vimeo.Player(iframe)
+  popupVideos.forEach(popupVideo => {
+    const play = popupVideo.querySelector('.button')
+    const close = popupVideo.querySelector('.icon-close')
+    const iframe = popupVideo.querySelector('iframe')
+    const player = new Vimeo.Player(iframe)
 
-  play.addEventListener('click', () => {
-    popupVideo.scrollIntoView({ behavior: 'smooth', block: 'center' })
-    document.body.classList.add('popup-video-playing')
-    player.play()
-  })
+    play.addEventListener('click', () => {
+      popupVideo.scrollIntoView({ behavior: 'smooth', block: 'center' })
+      document.body.classList.add('popup-video-playing')
+      player.play()
+    })
 
-  close.addEventListener('click', () => {
-    popupVideo.scrollIntoView({ behavior: 'smooth', block: 'center' })
-    player.pause()
-    document.body.classList.remove('popup-video-playing')
+    close.addEventListener('click', () => {
+      popupVideo.scrollIntoView({ behavior: 'smooth', block: 'center' })
+      player.pause()
+      document.body.classList.remove('popup-video-playing')
+    })
   })
 }
