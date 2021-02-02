5a0d6bae0129fc37626154f7a642445e166d5465
Daniel Clowes <daniel.clowes@intelygenz.com>
2021-02-02 17:00:20 +0100

    fix raf template

--- src/stories/layout-components/faq/faq-table.story.js
+++ src/stories/layout-components/faq/faq-table.story.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import FAQ, { FAQTables } from '../../../components/layout/FAQ'
-import { defaultItems } from '../../templates/rates-and-fees.story'
+import { defaultItems } from '../../templates/rates-and-fees'
 import { number, text, object } from '@storybook/addon-knobs'
 
 const options = {
