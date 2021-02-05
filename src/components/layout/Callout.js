767b18d16dc7cd11f37095bd5cc21660cdd40dd5
Daniel Clowes <daniel.clowes@intelygenz.com>
2021-02-05 13:24:49 +0100

    refactor callout component

--- src/components/layout/Callout.js
+++ src/components/layout/Callout.js
@@ -2,11 +2,14 @@ import React from 'react'
 import classNames from 'classnames'
 import Eyebrow from '../base/Eyebrow'
 
-const Callout = ({ eyebrow = {}, title, text, className }) => (
+const Callout = ({ eyebrow = {}, title, text, className, children }) => (
   <div className={classNames('callout container', className)}>
     {eyebrow.enable && <Eyebrow>{eyebrow.text}</Eyebrow>}
-    <h1 className='callout-title'>{title}</h1>
-    <p className='callout-text'>{text}</p>
+    <h1 className='callout-title' dangerouslySetInnerHTML={{ __html: title }} />
+    {text && (
+      <p className='callout-text' dangerouslySetInnerHTML={{ __html: text }} />
+    )}
+    {children && <div className='callout-content'>{children}</div>}
   </div>
 )
 
