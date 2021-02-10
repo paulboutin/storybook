94657a7848188cfa97b185e18d066e68c9f245b9
Daniel Clowes <daniel.clowes@intelygenz.com>
2021-02-10 10:51:50 +0100

    add dsm ids to resource links

--- src/stories/layout-components/resource-links/resource-links-a.story.js
+++ src/stories/layout-components/resource-links/resource-links-a.story.js
@@ -4,5 +4,10 @@ import ResourceLinksImageStory from './resource-links-image'
 export const ResourceLinksA = () => <ResourceLinksImageStory type='A' />
 
 export default {
-  title: 'Layout Components/Resource Links'
+  title: 'Layout Components/Resource Links',
+  parameters: {
+    'in-dsm': {
+      id: '6023ac59f2309e4b7560fdec'
+    }
+  }
 }
--- src/stories/layout-components/resource-links/resource-links-b.story.js
+++ src/stories/layout-components/resource-links/resource-links-b.story.js
@@ -4,5 +4,10 @@ import ResourceLinksImageStory from './resource-links-image'
 export const ResourceLinksB = () => <ResourceLinksImageStory type='B' />
 
 export default {
-  title: 'Layout Components/Resource Links'
+  title: 'Layout Components/Resource Links',
+  parameters: {
+    'in-dsm': {
+      id: '6023ac601b5a3bbf081b356b'
+    }
+  }
 }
