5e143a7617cde8e23a4bf1542b332e0638d19bdf
Daniel Clowes <daniel.clowes@intelygenz.com>
2021-02-24 11:24:31 +0100

    allow support for single/double columns in resource links standard

--- src/components/layout/ResourceLinks.js
+++ src/components/layout/ResourceLinks.js
@@ -2,11 +2,62 @@ import React from 'react'
 import Eyebrow from '../base/Eyebrow'
 import Link from '../base/Link'
 
-export const defaultEyebrow = 'Eyebrow'
-export const defaultTitle = 'Headline'
-export const defaultText = 'Lorem ipsum'
+export const defaultItems = [
+  'Savings & Money Markets',
+  'Business CDs',
+  'Account Services',
+  'Payroll Services',
+  'Credit Cards',
+  'Mortgages',
+  'International Banking'
+]
 
-const ResourceLinks = ({ eyebrow = {}, title, text }) => (
+export const defaultContent = {
+  eyebrow: 'Eyebrow',
+  title: 'Headline',
+  text: 'Lorem ipsum'
+}
+
+export const ResourceLinksDoubleColumns = ({ items }) => {
+  const left = items.slice(0, Math.ceil(items.length / 2))
+  const right = items.slice(Math.ceil(items.length / 2))
+
+  return (
+    <div className='row'>
+      <div className='col-xs-12 col-md-6'>
+        {left.map((item, idx) => (
+          <Link key={idx} href='#'>
+            {item}
+          </Link>
+        ))}
+      </div>
+
+      <div className='col-xs-12 col-md-6'>
+        {right.map((item, idx) => (
+          <Link key={idx} href='#'>
+            {item}
+          </Link>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export const ResourceLinksSingleColumn = ({ items }) => {
+  return (
+    <div className='row'>
+      <div className='col-xs-12'>
+        {items.map((item, idx) => (
+          <Link key={idx} href='#'>
+            {item}
+          </Link>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+const ResourceLinks = ({ eyebrow = {}, title, text, children }) => (
   <section className='resource-links container'>
     <div className='row'>
       <div className='col-xs-12 col-sm-6 col-md-4'>
@@ -24,20 +75,7 @@ const ResourceLinks = ({ eyebrow = {}, title, text }) => (
       </div>
 
       <div className='col-xs-12 col-sm-5 col-sm-offset-1 col-md-7'>
-        <div className='row'>
-          <div className='col-xs-12 col-md-6'>
-            <Link href='#'>Savings & Money Markets</Link>
-            <Link href='#'>Business CDs</Link>
-            <Link href='#'>Account Services</Link>
-            <Link href='#'>Payroll Services</Link>
-          </div>
-
-          <div className='col-xs-12 col-md-6'>
-            <Link href='#'>Savings & Money Markets</Link>
-            <Link href='#'>Business CDs</Link>
-            <Link href='#'>Account Services</Link>
-          </div>
-        </div>
+        {children}
       </div>
     </div>
   </section>
--- src/stories/layout-components/resource-links/resource-links-_standard.story.js
+++ src/stories/layout-components/resource-links/resource-links-_standard.story.js
@@ -1,23 +1,37 @@
 import React from 'react'
 import ResourceLinks, {
-  defaultEyebrow,
-  defaultText,
-  defaultTitle
+  ResourceLinksDoubleColumns,
+  ResourceLinksSingleColumn,
+  defaultContent,
+  defaultItems
 } from '../../../components/layout/ResourceLinks'
-import { boolean, text } from '@storybook/addon-knobs'
+import { boolean, text, radios, array } from '@storybook/addon-knobs'
+
+const options = {
+  '1': '1',
+  '2': '2'
+}
 
 export const ResourceLinksStandard = () => {
+  const columns = radios('Columns', options, '2')
   const enableEyebrow = boolean('Eyebrow Enable', true)
-  const eyebrow = text('Eyebrow Text', defaultEyebrow)
-  const title = text('Title', defaultTitle)
-  const textContent = text('Text', defaultText)
+  const eyebrow = text('Eyebrow Text', defaultContent.eyebrow)
+  const title = text('Title', defaultContent.title)
+  const textContent = text('Text', defaultContent.text)
+  const items = array('Items', defaultItems, ';')
 
   return (
     <ResourceLinks
       eyebrow={{ enable: enableEyebrow, text: eyebrow }}
       title={title}
       text={textContent}
-    />
+    >
+      {columns === '2' ? (
+        <ResourceLinksDoubleColumns items={items} />
+      ) : (
+        <ResourceLinksSingleColumn items={items} />
+      )}
+    </ResourceLinks>
   )
 }
 
