685382b7e42bd10fe1dc2c0be31dd60d4bfbfada
Daniel Clowes <daniel.clowes@intelygenz.com>
2021-03-02 18:59:20 +0100

    pre-render retail app highlight

--- src/components/base/AppCTA.js
+++ src/components/base/AppCTA.js
@@ -2,10 +2,16 @@ import React from 'react'
 
 const AppCTA = () => (
   <div className='app-cta'>
-    <a href='#' className='app-cta-download'>
+    <a
+      href='https://apps.apple.com/us/app/bank-of-the-west-mobile-app/id428713765?ls=1'
+      className='app-cta-download'
+    >
       <img src='/img/app-store.svg' alt='Download on the App Store' />
     </a>
-    <a href='#' className='app-cta-download'>
+    <a
+      href='https://play.google.com/store/apps/details?id=com.botw.mobilebanking'
+      className='app-cta-download'
+    >
       <img src='/img/google-play.svg' alt='Get it on Google Play' />
     </a>
   </div>
