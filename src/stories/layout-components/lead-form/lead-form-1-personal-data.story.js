7b1abe2847b6406154629e5f908bd48e891b36c6
Daniel Clowes <daniel.clowes@intelygenz.com>
2021-03-15 19:08:00 +0100

    add additional knobs to lead form

--- src/stories/layout-components/lead-form/lead-form-1-personal-data.story.js
+++ src/stories/layout-components/lead-form/lead-form-1-personal-data.story.js
@@ -1,15 +1,43 @@
 import React from 'react'
-import { text } from '@storybook/addon-knobs'
+import { boolean, text } from '@storybook/addon-knobs'
 import Button from '../../../components/base/Button'
 import Input from '../../../components/base/Input'
 import Link from '../../../components/base/Link'
 
+const defaultInputs = [
+  {
+    label: 'Company Name',
+    type: 'text'
+  },
+  {
+    label: 'Name',
+    type: 'text'
+  },
+  {
+    label: 'Email Address',
+    type: 'email'
+  },
+  {
+    label: 'Phone Number',
+    type: 'tel'
+  }
+]
+
 export const PersonalData = () => {
-  const headline = text('Headline', 'Headline')
-  const subtext = text('Subtext', 'Subtext')
-  const subHeadline = text('Subheadline', 'Subheadline')
-  const cta1 = text('CTA 1', 'Call To Action')
-  const cta2 = text('CTA 2', 'Call To Action')
+  const headline = text('Headline', 'Headline', 'Left Column')
+  const subtext = text('Subtext', 'Subtext', 'Left Column')
+  const subHeadline = text('Subheadline', 'Subheadline', 'Left Column')
+  const cta1 = text('CTA 1', 'Call To Action', 'Left Column')
+  const cta2 = text('CTA 2', 'Call To Action', 'Left Column')
+
+  const inputs = defaultInputs.map((input, idx) => {
+    const prefix = `Input ${idx + 1}`
+    return {
+      enable: boolean('Enable Input', true, prefix),
+      label: text('Input Label', input.label, prefix),
+      type: text('Input Type', input.type, prefix)
+    }
+  })
 
   return (
     <form className='lead-form container'>
@@ -19,15 +47,18 @@ export const PersonalData = () => {
       </div>
       <div className='lead-form-row row'>
         <div className='lead-form-input-group col-12 col-sm-6'>
-          <Input label='Company Name' variant='outline' required />
-          <Input label='Name' variant='outline' required />
-          <Input
-            label='Email Address'
-            type='email'
-            variant='outline'
-            required
-          />
-          <Input label='Phone Number' type='tel' variant='outline' required />
+          {inputs.map(
+            (input, idx) =>
+              input.enable && (
+                <Input
+                  key={idx}
+                  label={input.label}
+                  type={input.type}
+                  variant='outline'
+                  required
+                />
+              )
+          )}
           <Button label='Submit' type='primary' />
         </div>
         <div className='lead-form-text-group col-12 col-sm-6'>
