cb47384b83cbc3eb929ad380c1b829634cfd95cd
Daniel Clowes <daniel.clowes@intelygenz.com>
2021-03-18 12:54:01 +0100

    fix for images on xl screens

--- src/components/base/Image.js
+++ src/components/base/Image.js
@@ -3,8 +3,22 @@ import classNames from 'classnames'
 
 export const ratios = ['auto', '4:3', '16:9', '1:1', '3:4', '9:16']
 
-const Image = ({ src, alt, ratio = ratios[0], className }) => (
-  <div className={classNames(`ratio-${ratio}`, className)}>
+const Image = ({
+  desktopLimit = true,
+  src,
+  alt,
+  ratio = ratios[0],
+  className
+}) => (
+  <div
+    className={classNames(
+      `ratio-${ratio}`,
+      {
+        'desktop-size-limit': ratio !== 'auto' && desktopLimit
+      },
+      className
+    )}
+  >
     <img src={src} alt={alt} />
   </div>
 )
