a1ae83f9baa346a1ac0b7d6cfc4dc3af23d87b57
Daniel Clowes <daniel.clowes@intelygenz.com>
2021-03-22 18:26:26 +0100

    update assets

--- src/stories/layout-components/carousels/carousel-services.story.js
+++ src/stories/layout-components/carousels/carousel-services.story.js
@@ -13,28 +13,28 @@ const defaultItems = [
   {
     type: 'services-icon',
     heading: 'Individuals and Families',
-    image: '/img/wealth/BOTW_Wealth_Expertise_Individual.png',
+    image: '/img/wealth/expertise/BOTW_Wealth_Expertise_Individuals.svg',
     href:
       'https://www.bankofthewest.com/wealth-management/who-we-serve/individuals-families'
   },
   {
     type: 'services-icon',
     heading: 'High Net Worth',
-    image: '/img/wealth/BOTW_Wealth_Expertise_HighNet.png',
+    image: '/img/wealth/expertise/BOTW_Wealth_Expertise_NetWorth.svg',
     href:
       'https://www.bankofthewest.com/wealth-management/who-we-serve/key-client-group-america'
   },
   {
     type: 'services-icon',
     heading: 'Private Business and Entrepreneurs',
-    image: '/img/wealth/BOTW_Wealth_Expertise_PrivateBusiness.png',
+    image: '/img/wealth/expertise/BOTW_Wealth_Expertise_Business.svg',
     href:
       'https://www.bankofthewest.com/wealth-management/who-we-serve/private-business-owners'
   },
   {
     type: 'services-icon',
     heading: 'International',
-    image: '/img/wealth/BOTW_Wealth_Expertise_International.png',
+    image: '/img/wealth/expertise/BOTW_Wealth_Expertise_International.svg',
     href:
       'https://www.bankofthewest.com/commercial-banking/international/global-capabilities'
   }
